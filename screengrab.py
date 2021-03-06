2cb97d589e2f6b1e2bf09298ab8f60228fe53dc8
NickCheng0921 <nickshacheng.0921@gmail.com>
2021-03-06 00:26:42 -0800

    Added HoughLine Approximation
    I want to use a line finding algorithm to find the stage, then use one to find the characters
     - uses OpenCV hough algorithm to find lines, seems to eat up a ton of processing power as of right now
    - we can use the probability type and fiddle with the parameters to hopefully speed it up

--- screengrab.py
+++ screengrab.py
@@ -16,23 +16,34 @@ import time
 #get frames
 
 def process_img(image):
-    original_image = image
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # edge detection of images
     # threshold 1 is high threshold value of intensity gradient
     # threshold2 is low threshold value of intensity gradient
     # takes argument for aperture size for intensity gradient
-    processed_img =  cv2.Canny(processed_img, threshold1 = 100, threshold2=300)
+    processed_img =  cv2.Canny(processed_img, threshold1 = 50, threshold2=100)
+    #hough lines can detect shape even through some distortions
+    #make hough lines detect lines of stages
+    # args: input binary image, rho accuracy (distance), radian (step), threshold vote to be line
+    # extra args for HoughLinesP: min line length, max gap between line segments
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 40, 5)
+    draw_lines(processed_img, lines)
     return processed_img
+
+def draw_lines(img, lines):
+    #draw thicker lines over where we find lines
+    for line in lines:
+        coords = line[0]
+        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
 
 def main():
     print("Running Main")
     last_time = time.time()
     while True:
         #print("running")
-        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        #print('Frame took {} seconds'.format(time.time()-last_time))
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,1366,768)))
+        print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         new_screen = process_img(screen)
         cv2.imshow('window', new_screen)
