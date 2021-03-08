20d9575ee06c750e7ddf45f055a8ea399184cf30
NickCheng0921 <nickshacheng.0921@gmail.com>
2021-03-07 21:00:57 -0800

    Faster Screen Grab

--- screengrab.py
+++ screengrab.py
@@ -13,8 +13,9 @@ from PIL import ImageGrab
 import cv2
 import time
 
-#get frames
+from grabScreenFast import grab_screen
 
+#get frames
 def process_img(image):
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -27,26 +28,44 @@ def process_img(image):
     #make hough lines detect lines of stages
     # args: input binary image, rho accuracy (distance), radian (step), threshold vote to be line
     # extra args for HoughLinesP: min line length, max gap between line segments
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 40, 5)
-    draw_lines(processed_img, lines)
+    # step with np.pi/2 for 90 degree steps
+    # step with np.pi for 180 degree steps or vertical lines only
+    lines = []
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/2, 200, 40, 10)
+     #draw_lines(processed_img, lines, 3)
+    
+    #x, contours = cv2.findContours(processed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    #can't draw colored lines onto a binarized image
+    colored_img = cv2.cvtColor(processed_img, cv2.COLOR_GRAY2BGR)
+    draw_red_lines(colored_img, lines, 2)
+    cv2.imshow('window', colored_img)
+
     return processed_img
 
-def draw_lines(img, lines):
+def draw_lines(img, lines, thickness):
     #draw thicker lines over where we find lines
     for line in lines:
         coords = line[0]
-        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), (255, 255, 255), thickness)
+
+def draw_red_lines(img, lines, thiccy):
+    #draw red lines over input
+    for line in lines:
+        coords = line[0]
+        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), (0, 0, 255), thiccy)
 
 def main():
     print("Running Main")
     last_time = time.time()
     while True:
         #print("running")
-        screen =  np.array(ImageGrab.grab(bbox=(0,40,1366,768)))
+        #screen =  np.array(ImageGrab.grab(bbox=(0,50,1200,750)))
+        screen = grab_screen((0,50,1200,750))
         print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         new_screen = process_img(screen)
-        cv2.imshow('window', new_screen)
+        #cv2.imshow('window', new_screen)
         #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
